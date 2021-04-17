7a5ea7be64c87fbc1950917d84b5ce393f68f2bb
Sinthorn Pradutnam <psinthorn@imacs-iMac-2.local>
2021-04-17 15:53:29 +0700

    implement database repo to holds database connection

---
+++ internal/drivers/db_driver.go
@@ -0,0 +1,57 @@
+package db_driver
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+type DB struct {
+	SQL *sql.DB
+}
+
+var dbConn = &DB{}
+
+const maxDBConn = 10
+const maxDBIdleConn = 5
+const maxDBLifeTime = 5 * time.Minute
+
+func ConnectSQL(dsn string) (*DB, error) {
+	db, err := NewDatabase(dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetMaxOpenConns(maxDBConn)
+	db.SetMaxIdleConns(maxDBIdleConn)
+	db.SetConnMaxLifetime(maxDBLifeTime)
+
+	dbConn.SQL = db
+	return dbConn, nil
+}
+
+func testDB(db *sql.DB) error {
+	err := db.Ping()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+
+}
