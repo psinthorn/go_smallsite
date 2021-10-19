b3e52dd6e73544bfd6d4964ff73702958b9ab677
Sinthorn <psinthorn@gmail.com>
2021-10-19 10:29:46 +0700

    fixed auth notice

--- internal/utils/is_authenthicated.go
+++ internal/utils/is_authenthicated.go
@@ -7,11 +7,11 @@ import (
 // IsAuthenticated checked user logged in or not if logged will return true
 func (u *Utils) IsAuthenticated(r *http.Request) bool {
 	var exists bool
-	if appConfig.IsProduction == false {
-		exists = false
-	} else {
-		exists = appConfig.Session.Exists(r.Context(), "user_id")
-	}
-	//exists := appConfig.Session.Exists(r.Context(), "user_id")
+	// if appConfig.IsProduction == false {
+	// 	exists = true
+	// } else {
+	// 	exists = appConfig.Session.Exists(r.Context(), "user_id")
+	// }
+	exists = appConfig.Session.Exists(r.Context(), "user_id")
 	return exists
 }
